f42e7a36d61008b8f6fd2bdaea6d0f94ff8e5e4b
Ives <jianxing.imo99g@gmail.com>
2023-04-09 17:50:11 +0800

    feat: 說明隱式等待的設置位置

---
+++ src/test/java/waittype/ImplicitWaitDemoTest.java
@@ -0,0 +1,43 @@
+package waittype;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import java.time.Duration;
+
+@Slf4j
+public class ImplicitWaitDemoTest {
+
+    WebDriver driver;
+    String baseUrl;
+
+    @Before
+    public void setUp(){
+        driver = new EdgeDriver();
+        baseUrl = "https://tw.yahoo.com";
+        driver.manage().window().maximize();
+        // 此為隱式等待的設置位置
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+    }
+
+    @Test
+    public void test(){
+        log.info("開啟網頁");
+        driver.get(baseUrl);
+        log.info("點擊登入頁");
+        driver.findElement(By.id("header-signin-link")).click();
+        log.info("輸入登入資訊");
+        driver.findElement(By.id("login-username")).sendKeys("sample@yahoo.com.tw");
+    }
+
+    @After
+    public void tearDown() throws Exception{
+        Thread.sleep(2000);
+        driver.quit();
+    }
+}
